6bb5c00c66b45a9b8585c946ad534a6e706868d3
Bastiaan Quast <bquast@gmail.com>
2014-05-23 13:28:28 +0200

    cleanup

--- run_analysis.R
+++ run_analysis.R
@@ -1,17 +1,12 @@
+# Bastiaan Quast
+# bquast@gmail.com
+
 # analysis
 
 # load the datasets
 load("~/Oil-MIDs/midi.2002.2010.RData")
 load("~/Oil-MIDs/midip.2002.2010.RData")
 
-# inspect the data
-View(midi)
-View(midip)
-ls(midi)
-ls(midip)
-
-
-### A short lookup script ###
 # load the functions
 source("functions.R")
 
